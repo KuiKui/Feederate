385b44103cce0e69a4f12757125ce74d9854854d
Florent Dubost <florent.dubost@gmail.com>
2014-01-10 15:19:17 +0100

    FOSJsRoutingBundle + Restangular

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,144 +1,157 @@
-angular.module('truncate', [])
-    .filter('characters', function () {
-        return function (input, chars, breakOnWord) {
-            if (isNaN(chars)) return input;
-            if (chars <= 0) return '';
-            if (input && input.length >= chars) {
-                input = input.substring(0, chars);
+(function () {
+    'use strict';
 
-                if (!breakOnWord) {
-                    var lastspace = input.lastIndexOf(' ');
-                    //get last space
-                    if (lastspace !== -1) {
-                        input = input.substr(0, lastspace);
+    angular.module('truncate', [])
+        .filter('characters', function () {
+            return function (input, chars, breakOnWord) {
+                if (isNaN(chars)) return input;
+                if (chars <= 0) return '';
+                if (input && input.length >= chars) {
+                    input = input.substring(0, chars);
+
+                    if (!breakOnWord) {
+                        var lastspace = input.lastIndexOf(' ');
+                        //get last space
+                        if (lastspace !== -1) {
+                            input = input.substr(0, lastspace);
+                        }
+                    }else{
+                        while(input.charAt(input.length-1) == ' '){
+                            input = input.substr(0, input.length -1);
+                        }
                     }
-                }else{
-                    while(input.charAt(input.length-1) == ' '){
-                        input = input.substr(0, input.length -1);
+                    return input + '...';
+                }
+                return input;
+            };
+        })
+        .filter('words', function () {
+            return function (input, words) {
+                if (isNaN(words)) return input;
+                if (words <= 0) return '';
+                if (input) {
+                    var inputWords = input.split(/\s+/);
+                    if (inputWords.length > words) {
+                        input = inputWords.slice(0, words).join(' ') + '...';
                     }
                 }
-                return input + '...';
-            }
-            return input;
-        };
-    })
-    .filter('words', function () {
-        return function (input, words) {
-            if (isNaN(words)) return input;
-            if (words <= 0) return '';
-            if (input) {
-                var inputWords = input.split(/\s+/);
-                if (inputWords.length > words) {
-                    input = inputWords.slice(0, words).join(' ') + '...';
+                return input;
+            };
+        });
+
+    var app = angular.module('feederate', ['ngSanitize', 'truncate', 'restangular']);
+
+    app.directive('ngEnter', function() {
+        return function(scope, element, attrs) {
+            element.bind("keydown keypress", function(event) {
+                if(event.which === 13) {
+                    scope.$apply(function(){
+                        scope.$eval(attrs.ngEnter);
+                    });
+
+                    event.preventDefault();
                 }
-            }
-            return input;
+            });
         };
     });
 
-var app = angular.module('feederate', ['ngResource', 'ngSanitize', 'truncate']);
+    app.controller('BoardCtrl', function BoardCtrl ($scope, Restangular) {
+        $scope.activeFeed    = null;
+        $scope.activeSummary = null;
+        $scope.entries       = [];
 
-app.factory('Constants', [
-    function() {
-        return {
-            RESOURCE_URL: "/app_dev.php/api"
-        }
-    }
-]);
-
-app.factory('Rest', ['Constants', '$resource', function(C, $resource) {
-    return {
-        Feeds: $resource(C.RESOURCE_URL + '/feeds'),
-        Entries: $resource(C.RESOURCE_URL + '/feeds/:feedId/entries', {feedId:'@id'}),
-        Summaries: $resource(C.RESOURCE_URL + '/feeds/:feedId/summaries', {feedId:'@id'}),
-        readSummary: $resource(C.RESOURCE_URL + '/summaries/:id/read', {id:'@id'}),
-        starSummary: $resource(C.RESOURCE_URL + '/summaries/:id/star', {id:'@id'}),
-        parseFeed: $resource(C.RESOURCE_URL + '/feeds/:feedId/parse', {feedId:'@id'})
-    }
-}]);
-
-app.directive('ngEnter', function() {
-    return function(scope, element, attrs) {
-        element.bind("keydown keypress", function(event) {
-            if(event.which === 13) {
-                scope.$apply(function(){
-                    scope.$eval(attrs.ngEnter);
+        $scope.addFeed = function () {
+            Restangular
+                .all(getRoute('get_feeds'))
+                .post({
+                    title: $scope.newFeedUrl,
+                    url: $scope.newFeedUrl,
+                    targetUrl: $scope.newFeedUrl
+                })
+                .then(function (feed) {
+                    feed
+                        .customGET('parse')
+                        .then(function() {
+                            $scope.newFeedUrl = '';
+                            $scope.loadFeeds();
+                            $scope.activeFeed = feed;
+                        });
                 });
+        };
 
-                event.preventDefault();
+        $scope.loadFeeds = function () {
+            Restangular
+                .all(getRoute('get_feeds'))
+                .getList()
+                .then(function(feeds) {
+                    $scope.feeds = feeds;
+                })
+        };
+
+        $scope.isActiveFeed = function (feed) {
+            return angular.equals(feed, $scope.activeFeed);
+        };
+
+        $scope.loadSummaries = function (feed) {
+            Restangular
+                .one(getRoute('get_feeds'), feed.id)
+                .getList('summaries')
+                .then(function(summaries) {
+                    $scope.summaries     = summaries;
+                    $scope.activeFeed    = feed;
+                    $scope.activeSummary = null;
+
+                    $scope.loadEntries(feed);
+                })
+        };
+
+        $scope.isActiveSummary = function (summary) {
+            return angular.equals(summary, $scope.activeSummary);
+        };
+
+        $scope.loadEntries = function (feed) {
+            Restangular
+                .one(getRoute('get_feeds'), feed.id)
+                .getList('entries')
+                .then(function(entries) {
+                    angular.forEach(entries, function(entry) {
+                        $scope.entries[entry.id] = entry;
+                    });
+                });
+        };
+
+        $scope.loadEntry = function (summary) {
+            $scope.entry         = $scope.entries[summary.id];
+            $scope.activeSummary = summary;
+        };
+
+        $scope.markAsRead = function (summary) {
+            if (!summary.is_read) {
+                Restangular
+                    .oneUrl(getRoute('post_summary_summaries_read', {id: summary.id}))
+                    .customPOST({is_read: true});
+
+                summary.is_read = true;
+                $scope.activeFeed.unread_count--;
             }
-        });
-    };
-});
+        };
 
-app.controller('BoardCtrl', ['$scope', 'Rest', function BoardCtrl ($scope, Rest) {
-    $scope.activeFeed    = null;
-    $scope.activeSummary = null;
-    $scope.entries       = [];
+        $scope.markAsStarred = function (summary) {
+            Restangular
+                .oneUrl(getRoute('post_summary_summaries_star', {id: summary.id}))
+                .customPOST({is_starred: !summary.is_starred});
 
-    $scope.addFeed = function () {
-        Rest.Feeds.save({
-            title: $scope.newFeedUrl,
-            url: $scope.newFeedUrl,
-            targetUrl: $scope.newFeedUrl
-        }, function (feed) {
-            Rest.parseFeed.get({feedId: feed.id}, function () {
-                $scope.newFeedUrl = '';
-                $scope.loadFeeds();
-                $scope.activeFeed = feed;
-            });
-        });
-    };
+            summary.is_starred = !summary.is_starred;
+        };
 
-    $scope.loadFeeds = function () {
-        Rest.Feeds.query(function (feeds) {
-            $scope.feeds = feeds;
-        });
-    };
+        $scope.loadFeeds();
+    });
 
-    $scope.isActiveFeed = function (feed) {
-        return angular.equals(feed, $scope.activeFeed);
-    };
-
-    $scope.loadSummaries = function (feed) {
-        Rest.Summaries.query({feedId: feed.id}, function (summaries) {
-            $scope.summaries     = summaries;
-            $scope.activeFeed    = feed;
-            $scope.activeSummary = null;
-
-            $scope.loadEntries(feed);
-        });
-    };
-
-    $scope.isActiveSummary = function (summary) {
-        return angular.equals(summary, $scope.activeSummary);
-    };
-
-    $scope.loadEntries = function (feed) {
-        Rest.Entries.query({feedId: feed.id}, function (entries) {
-            for (var index in entries) {
-                $scope.entries[entries[index].id] = entries[index];
-            }
-        });
-    };
-
-    $scope.loadEntry = function (summary) {
-        $scope.entry         = $scope.entries[summary.id];
-        $scope.activeSummary = summary;
-    };
-
-    $scope.markAsRead = function (summary) {
-        if (!summary.is_read) {
-            Rest.readSummary.save({id: summary.id}, {is_read: true});
-            summary.is_read = true;
-            $scope.activeFeed.unread_count--;
+    var getRoute = function(routeName, routeParams) {
+        if (routeParams === 'undefined') {
+            var routeParams = {};
         }
-    };
 
-    $scope.markAsStarred = function (summary) {
-        Rest.starSummary.save({id: summary.id}, {is_starred: !summary.is_starred});
-        summary.is_starred = !summary.is_starred;
-    };
-
-    $scope.loadFeeds();
-}]);
+        return Routing.generate(routeName, routeParams, false).slice(1);
+    }
+})();
