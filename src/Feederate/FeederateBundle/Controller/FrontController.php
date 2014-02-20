e08abc12be31c4f62197ed7170e6e8936cb58582
Florent Dubost <florent.dubost@gmail.com>
2014-02-20 16:18:41 +0100

    Init feedback

--- src/Feederate/FeederateBundle/Controller/FrontController.php
+++ src/Feederate/FeederateBundle/Controller/FrontController.php
@@ -4,9 +4,9 @@ namespace Feederate\FeederateBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Feederate\FeederateBundle\Form\FeedBackType;
 
 class FrontController extends Controller
 {
@@ -24,6 +24,48 @@ class FrontController extends Controller
         } else if ($securityContext->isGranted('IS_AUTHENTICATED_ANONYMOUSLY')) {
             return $this->homeAction();
         }
+    }
+
+    /**
+     * Feedback action
+     *
+     * @Route("/feedback")
+     */
+    public function feedbackAction()
+    {
+        $form = $this->createForm(new FeedBackType());
+
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST') {
+            $form->submit($request);
+
+            if ($form->isValid()) {
+                
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('[Feedback]')
+                    ->setFrom($form->getData()['email'])
+                    ->setTo('team@feederate.me')
+                    ->setBody($form->getData()['message']);
+
+                $this->get('mailer')->send($message);
+
+                return $this->redirect($this->generateUrl('feederate_feederate_front_feedbackthanks'));
+            }
+        }
+
+        return $this->render('FeederateFeederateBundle:Front:feedback.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * Feedback action
+     *
+     * @Route("/feedback_thanks")
+     */
+    public function feedbackThanksAction()
+    {
+        return $this->render('FeederateFeederateBundle:Front:feedbackThanks.html.twig');
     }
 
     protected function homeAction()
