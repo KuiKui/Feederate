c21761182d87739cb60f2a0d0f82c43940963f32
Morgan Brunot <brunot.morgan@gmail.com>
2014-02-26 14:55:16 +0100

    create entries factory

--- src/Feederate/FeederateBundle/Resources/public/js/controller.js
+++ src/Feederate/FeederateBundle/Resources/public/js/controller.js
@@ -28,7 +28,7 @@
             list: {},
             unread: {},
             starred: {},
-            active: {},
+            active: null,
         };
 
         /**
@@ -88,36 +88,187 @@
             return angular.equals(feed, Feeds.unread);
         };
 
+        Feeds.isStarred = function (feed) {
+            return angular.equals(feed, Feeds.starred);
+        };
+
+        Feeds.type = function (feed) {
+            if (angular.equals(feed, Feeds.starred)) {
+                return 'starred';
+            } else if (angular.equals(feed, Feeds.unread)) {
+                return 'unread';
+            }
+
+            return '';
+        };
+
         return Feeds;
+    });
+
+    feederate.factory('Entries', function (Restangular, Router, Feeds, $filter) {
+        var Entries = {
+            entriesList: {},
+            summariesList: {},
+            daysList: [],
+            active: null,
+            noMore: false,
+            areLoading: false,
+            page: 0
+        };
+
+        Entries.reset = function () {
+            Entries.summariesList = {};
+            Entries.daysList      = [];
+            Entries.activeSummary = null;
+            Entries.activeEntry   = null;
+            Entries.noMore        = false;
+            Entries.page          = 0;
+        };
+
+        Entries.loadSummaries = function (feed, callback) {
+
+            // If the load is in progress or if there are no more pages
+            if (Entries.areLoading || Entries.noMore) {
+                return;
+            }
+
+            var request = null,
+                type    = Feeds.type(feed);
+
+            if (!angular.equals(Feeds.active, feed)) {
+                Entries.reset();
+                Feeds.active = feed;
+            }
+
+            Entries.areLoading = true;
+
+            if (type === 'unread' || type === 'starred') {
+                request = Restangular.all(Router.get('get_summaries')).getList({
+                    type: type,
+                    page: Entries.page + 1
+                });
+            } else {
+                request = Restangular.one(Router.get('get_feeds'), feed.id).getList('summaries', {
+                    page: Entries.page + 1
+                });
+            }
+
+            request.then(function (summaries) {
+                Entries.areLoading = false;
+
+                // Last page ?
+                if (summaries.length == 0) {
+                    Entries.noMore = true;
+
+                    return;
+                }
+
+                angular.forEach(summaries, function (summary) {
+                    var day = $filter('formatDate')(summary.generated_at, 'YYYY-MM-DD');
+                    if (Entries.daysList.indexOf(day) === -1) {
+                        Entries.daysList.push(day);
+                        Entries.summariesList[day] = [];
+                    }
+
+                    Entries.summariesList[day].push(summary);
+                });
+
+                Entries.page++;
+
+                if (callback !== undefined) {
+                    callback();
+                }
+            });
+        };
+
+        Entries.loadEntries = function (feed) {
+            var request = null,
+                type    = Feeds.type(feed);
+
+            if (type === 'unread' || type === 'starred') {
+                request = Restangular.all(Router.get('get_entries')).getList({
+                    type: type,
+                    page: Entries.page
+                });
+            } else {
+                request = Restangular.one(Router.get('get_feeds'), feed.id).getList('entries', {
+                    page: Entries.page
+                });
+            }
+
+            request.then(function (entries) {
+                angular.forEach(entries, function (entry) {
+                    Entries.entriesList[entry.id] = entry;
+                });
+            });
+        };
+
+        Entries.markAsRead = function (summary, canToggled) {
+            canToggled = typeof canToggled !== 'undefined' ? canToggled : false;
+
+            if (canToggled || (!canToggled && !summary.is_read)) {
+                Restangular
+                    .oneUrl(Router.get('post_summary_summaries_read', {id: summary.id}))
+                    .customPOST({is_read: !summary.is_read});
+
+                summary.is_read = !summary.is_read;
+
+                if (summary.is_read) {
+                    Feeds.list[summary.feed_id].unread_count--;
+                    Feeds.unread.unread_count--;
+                } else {
+                    Feeds.list[summary.feed_id].unread_count++;
+                    Feeds.unread.unread_count++;
+                }
+            }
+        };
+
+        Entries.markAsStarred = function (summary) {
+            Restangular
+                .oneUrl(Router.get('post_summary_summaries_star', {id: summary.id}))
+                .customPOST({is_starred: !summary.is_starred});
+
+            summary.is_starred = !summary.is_starred;
+
+            if (summary.is_starred) {
+                Feeds.starred.unread_count++;
+            } else {
+                Feeds.starred.unread_count--;
+            }
+        };
+
+        Entries.loadIsBusy = function () {
+            return Entries.noMore || Entries.areLoading;
+        };
+
+        Entries.isActiveSummary = function (summary) {
+            return angular.equals(summary, Entries.activeSummary);
+        };
+
+        return Entries;
     });
 
 
 
     angular
         .module('feederate')
-        .controller('BoardCtrl', function BoardCtrl ($scope, $filter, Router, Feeds, Restangular, $location, $anchorScroll) {
+        .controller('BoardCtrl', function BoardCtrl ($scope, Router, Feeds, Entries, Restangular, $location, $anchorScroll) {
 
         angular.element(document).ready(function () {
-            $scope.activeSummary       = null;
-            $scope.selectedType        = 'summaries';
-            $scope.summariesAreLoading = false;
-            $scope.noMoreSummary       = false;
-            $scope.currentPage         = 0;
-            $scope.entries             = [];
-            $scope.summariesDays       = [];
-            $scope.summaries           = {};
+            $scope.selectedType = 'summaries';
 
-            $scope.Feeds = Feeds;
+            $scope.Feeds   = Feeds;
+            $scope.Entries = Entries;
 
             $scope.loadFeeds = function (callback) {
-                $scope.Feeds.load(callback);
+                Feeds.load(callback);
             };
 
             $scope.addFeed = function () {
-                $scope.Feeds.add($scope.newFeedUrl, function (feed) {
+                Feeds.add($scope.newFeedUrl, function (feed) {
                     $scope.newFeedUrl = '';
-                    $scope.Feeds.load(function () {
-                        $scope.loadSummaries($scope.Feeds.list[feed.id]);
+                    Feeds.load(function () {
+                        $scope.loadSummaries(Feeds.list[feed.id]);
 
                         // Auto scroll into active feed
                         $location.hash('feed_' + feed.id);
@@ -128,9 +279,9 @@
 
             $scope.deleteFeed = function (feed) {
                 if (confirm('Do you really want delete feed "' + feed.title + '" ?')) {
-                    $scope.Feeds.delete(feed, function () {
-                        $scope.Feeds.load(function () {
-                            $scope.loadSummaries($scope.Feeds.unread);
+                    Feeds.delete(feed, function () {
+                        Feeds.load(function () {
+                            $scope.loadSummaries(Feeds.unread);
 
                             // Auto scroll into active feed
                             $location.hash('feed_unread');
@@ -142,30 +293,30 @@
 
             $scope.markFeedAsRead = function (feed) {
                 if (confirm('Do you really want mark all entries of "' + feed.title + '" as read ?')) {
-                    $scope.Feeds.markAsRead(feed, function () {
-                        if (!$scope.Feeds.isStarred(feed)) {
-                            if (!$scope.Feeds.isUnread(feed)) {
-                                $scope.Feeds.unread.unread_count       -= $scope.Feeds.list[feed.id].unread_count;
-                                $scope.Feeds.list[feed.id].unread_count = 0;
+                    Feeds.markAsRead(feed, function () {
+                        if (!Feeds.isStarred(feed)) {
+                            if (!Feeds.isUnread(feed)) {
+                                Feeds.unread.unread_count       -= Feeds.list[feed.id].unread_count;
+                                Feeds.list[feed.id].unread_count = 0;
                             } else {
 
                                 // All unread_count must be null
-                                $scope.Feeds.unread.unread_count = 0;
-                                angular.forEach($scope.Feeds.list, function (feed) {
+                                Feeds.unread.unread_count = 0;
+                                angular.forEach(Feeds.list, function (feed) {
                                     feed.unread_count = 0;
                                 });
                             }
 
-                            angular.forEach($scope.summariesDays, function (day) {
-                                angular.forEach($scope.summaries[day], function (summary) {
+                            angular.forEach(Entries.daysList, function (day) {
+                                angular.forEach(Entries.summariesList[day], function (summary) {
                                     summary.is_read = true;
                                 });
                             });
                         } else {
 
                             // We reload feeds because it's too complex to manage unread_count
-                            $scope.Feeds.load(function () {
-                                $scope.loadSummaries($scope.Feeds.starred);
+                            Feeds.load(function () {
+                                $scope.loadSummaries(Feeds.starred);
 
                                 // Auto scroll into active feed
                                 $location.hash('feed_' + feed.id);
@@ -176,138 +327,30 @@
                 }
             };
 
-            $scope.loadSummaries = function (feed, resetFeed) {
-                var summaries, type;
-
-                if (resetFeed = (resetFeed === undefined ? true : resetFeed)) {
-                    $scope.summaries     = {};
-                    $scope.summariesDays = [];
-                    $scope.currentPage   = 0;
-                    $scope.noMoreSummary = false;
-                    $scope.activeSummary = null;
-                }
-
-                if ($scope.summariesAreLoading || $scope.noMoreSummary) {
-                    return;
-                }
-
-                $scope.summariesAreLoading = true;
-                $scope.Feeds.active        = feed;
-
-                if (type = getFeedType(feed)) {
-                    summaries = Restangular
-                        .all(Router.get('get_summaries'))
-                        .getList({type: type, page: $scope.currentPage + 1});
-                } else {
-                    summaries = Restangular
-                        .one(Router.get('get_feeds'), feed.id)
-                        .getList('summaries', {page: $scope.currentPage + 1});
-                }
-
-                summaries.then(function (summaries) {
-                    $scope.summariesAreLoading = false;
-
-                    if (!summaries.length) {
-                        $scope.noMoreSummary = true;
-                        return;
-                    }
-
-                    angular.forEach(summaries, function (summary) {
-                        var day = $filter('formatDate')(summary.generated_at, 'YYYY-MM-DD');
-                        if ($scope.summariesDays.indexOf(day) === -1) {
-                            $scope.summariesDays.push(day);
-                            $scope.summaries[day] = [];
-                        }
-                        $scope.summaries[day].push(summary);
-                    });
-
-                    $scope.currentPage++;
+            $scope.loadSummaries = function (feed) {
+                Entries.loadSummaries(feed, function () {
                     $scope.loadEntries(feed);
                 });
             };
 
-            $scope.summaryLoadIsBusy = function () {
-                return $scope.noMoreSummary || $scope.summariesAreLoading;
-            };
-
-            $scope.isActiveSummary = function (summary) {
-                return angular.equals(summary, $scope.activeSummary);
-            };
-
             $scope.loadEntries = function (feed) {
-                var entries, type;
-
-                if (type = getFeedType(feed)) {
-                    entries = Restangular
-                        .all(Router.get('get_entries'))
-                        .getList({type: type, page: $scope.currentPage});
-                } else {
-                    entries = Restangular
-                        .one(Router.get('get_feeds'), feed.id)
-                        .getList('entries', {page: $scope.currentPage});
-                }
-
-                entries.then(function (entries) {
-                    angular.forEach(entries, function (entry) {
-                        $scope.entries[entry.id] = entry;
-                    });
-                });
+                Entries.loadEntries(feed);
             };
 
             $scope.loadEntry = function (summary) {
-                $scope.entry         = $scope.entries[summary.id];
-                $scope.activeSummary = summary;
+                Entries.activeSummary = summary;
+                Entries.activeEntry   = Entries.entriesList[summary.id];
 
                 // reset scroll entry
                 $('#entry .entry-content').scrollTop(0);
             };
 
-            $scope.markAsRead = function (summary, onlyUnread) {
-                if (onlyUnread === undefined || !onlyUnread || (onlyUnread && !summary.is_read)) {
-                    Restangular
-                        .oneUrl(Router.get('post_summary_summaries_read', {id: summary.id}))
-                        .customPOST({is_read: !summary.is_read});
-
-                    summary.is_read = !summary.is_read;
-
-                    if (summary.is_read) {
-                        $scope.Feeds.list[summary.feed_id].unread_count--;
-                        $scope.Feeds.unread.unread_count--;
-                    } else {
-                        $scope.Feeds.list[summary.feed_id].unread_count++;
-                        $scope.Feeds.unread.unread_count++;
-                    }
-                }
+            $scope.markAsRead = function (summary, canToggled) {
+                Entries.markAsRead(summary, canToggled)
             };
 
             $scope.markAsStarred = function (summary) {
-                Restangular
-                    .oneUrl(Router.get('post_summary_summaries_star', {id: summary.id}))
-                    .customPOST({is_starred: !summary.is_starred});
-
-                summary.is_starred = !summary.is_starred;
-
-                if (summary.is_starred) {
-                    $scope.Feeds.starred.unread_count++;
-                } else {
-                    $scope.Feeds.starred.unread_count--;
-                }
-            };
-
-            var getFeedType = function (feed) {
-                if (angular.equals(feed, $scope.Feeds.starred)) {
-                    return 'starred';
-                } else if (angular.equals(feed, $scope.Feeds.unread)) {
-                    return 'unread';
-                } else {
-                    return '';
-                }
-            };
-
-            $scope.sortMe = function () {
-                return function (object) {
-                    return object.title;
-                }
+                Entries.markAsStarred(summary);
             };
 
             $scope.$watch(function (){
@@ -325,7 +368,7 @@
             });
 
             $scope.loadFeeds(function () {
-                $scope.loadSummaries($scope.Feeds.unread);
+                $scope.loadSummaries(Feeds.unread);
             });
         });
     });
