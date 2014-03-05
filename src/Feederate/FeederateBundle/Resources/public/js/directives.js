0079d5fd3246e03bad8797b1d69ba3532a6661a7
Florent Dubost <florent.dubost@gmail.com>
2014-03-05 14:52:29 +0100

    Scroll correction

--- src/Feederate/FeederateBundle/Resources/public/js/directives.js
+++ src/Feederate/FeederateBundle/Resources/public/js/directives.js
@@ -16,4 +16,20 @@
             });
         };
     });
+
+    app.directive('entryContentBind',  ['$sce', '$parse', function($sce, $parse) {
+        return function(scope, element, attr) {
+            element.addClass('ng-binding').data('$binding', attr.entryContentBind);
+
+            var parsed = $parse(attr.entryContentBind);
+            function getStringValue() { return (parsed(scope) || '').toString(); }
+
+            scope.$watch(getStringValue, function ngBindHtmlWatchAction(value) {
+                element.html($sce.getTrustedHtml(parsed(scope)) || '');
+
+                // reset scroll entry
+                element.parents('#entry .entry-content').scrollTop(0);
+            });
+        };
+    }]);
 })();
